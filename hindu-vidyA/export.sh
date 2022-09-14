f7bd6ef871648b626f3efce06b9030cbc2ee797c
Vishvas Vasuki विश्वासः <vishvas.vasuki@gmail.com>
2022-09-14 10:11:05 +0530

    CU

---
+++ hindu-vidyA/export.sh
@@ -0,0 +1,2 @@
+discord-chat-exporter-cli exportguild -o ~/hindu-comm/hindu-dicord/hindu-vidyA/dump_json  -f Json -t $BOT_TOKEN -g 1017076634253283488
+discord-chat-exporter-cli exportguild -o ~/hindu-comm/hindu-dicord/hindu-vidyA/dump_html  -f Json -t $BOT_TOKEN -g 1017076634253283488
